9afd2d9b704d32efa17434da30e21fe4c3cf2bbd
Randel Yumul <randel.angelo10@gmail.com>
2025-11-21 21:05:41 +0800

    Skills adjusted for mobile (2).

--- src/components/molecules/SkillCard.tsx
+++ src/components/molecules/SkillCard.tsx
@@ -8,13 +8,13 @@ interface SkillCardProps {
 
 export const SkillCard = ({ title, items }: SkillCardProps) => {
   return (
-    <Card className="h-full p-10 bg-white dark:bg-black">
+    <Card className="h-auto p-6 sm:p-8 lg:p-10 bg-white dark:bg-black">
       <CardHeader>
-        <CardTitle className="font-bold text-lg md:text-xl lg:text-2xl text-center text-[#1da1f2]">
+        <CardTitle className="font-bold text-lg sm:text-xl lg:text-2xl text-center text-[#1da1f2]">
           {title}
         </CardTitle>
       </CardHeader>
-      <div className="border-b-2 border-black dark:border-white w-full"></div>
+      <div className="border-b-2 border-black dark:border-white w-full mb-4"></div>
       <CardContent className="text-left space-y-3">
         <ListAtom items={items} />
       </CardContent>
--- src/components/molecules/SkillsGrid.tsx
+++ src/components/molecules/SkillsGrid.tsx
@@ -4,7 +4,7 @@ interface SkillsGridProps {
 
 export const SkillsGrid = ({ children }: SkillsGridProps) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {children}
     </div>
   );
